960318b2b915654632924d6bef51a2a234590aeb
Alain-Le-Brun <ek991135@ens.uqam.ca>
2020-03-16 13:21:44 -0400

    message 2

---
+++ projet/src/main/java/Vecteur.java
@@ -0,0 +1,12 @@
+public class Vecteur{
+    public int nb_a = 0;
+    public int nb_b = 0;
+    public Vecteur(){
+        this.nb_a = 0;
+        this.nb_b = 0;
+    }
+    public Vecteur(int nb_1, int nb_2){
+        this.nb_b = nb_1;
+        this.nb_b = nb_2;
+    }
+}
