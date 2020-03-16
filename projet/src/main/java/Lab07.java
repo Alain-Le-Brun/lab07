aedf95de9a576f86efee58dfbb78a337898f9e2f
Alain-Le-Brun <ek991135@ens.uqam.ca>
2020-03-16 14:39:29 -0400

    message 4

--- projet/src/main/java/Lab07.java
+++ projet/src/main/java/Lab07.java
@@ -1,5 +1,12 @@
 public class Lab07 {
     public static void main(String args[]){
-
+        Vecteur vect_1 = new Vecteur();
+        Vecteur vect_2 = new Vecteur(3, 4);
+        vect_1.affVecteur();
+        vect_1.add(vect_2);
+        Vecteur vect_3 = new Vecteur(2, -2);
+        vect_1.affVecteur();
+        vect_1.add(vect_3);
+        vect_1.affVecteur();
     }
 }
--- projet/src/main/java/Vecteur.java
+++ projet/src/main/java/Vecteur.java
@@ -6,7 +6,24 @@ public class Vecteur{
         this.nb_b = 0;
     }
     public Vecteur(int nb_1, int nb_2){
-        this.nb_b = nb_1;
+        this.nb_a = nb_1;
         this.nb_b = nb_2;
+    }
+    public int getComp_1(){
+        return this.nb_a;
+    }
+    public int getComp_2(){
+        return this.nb_b;
+    }
+    public void add(Vecteur vecEntree){
+        this.nb_a = this.nb_a + vecEntree.getComp_1();
+        this.nb_b = this.nb_b + vecEntree.getComp_2();
+    }
+    public String enChaine(){
+        String sortie = "(" + nb_a + ", " + nb_b + ")"  + "\n";
+        return sortie;
+    }
+    public void affVecteur(){
+        System.out.print(this.enChaine());
     }
 }
